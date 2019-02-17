4d17389615bb0d718cca799a09aafce2a2492a76
Aleksey Zubakov <aleks.zubakov@jetbrains.com>
2019-02-17 22:03:34 +0300

    Compiler runner util

---
+++ klib/src/main/kotlin/org/jetbrains/kotlin/cli/klib/merger/CompilerRunner.kt
@@ -0,0 +1,38 @@
+package org.jetbrains.kotlin.cli.klib.merger
+
+import org.jetbrains.kotlin.cli.bc.K2Native
+import org.jetbrains.kotlin.cli.common.CLICompiler
+import org.jetbrains.kotlin.cli.common.CLITool
+import org.jetbrains.kotlin.cli.common.ExitCode
+import org.jetbrains.kotlin.konan.file.File
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+
+private fun executeCompiler(compiler: CLITool<*>, args: List<String>): Pair<String, ExitCode> {
+    val bytes = ByteArrayOutputStream()
+    val origErr = System.err
+    try {
+        System.setErr(PrintStream(bytes))
+        val exitCode = CLITool.doMainNoExit(compiler, args.toTypedArray())
+        return Pair(String(bytes.toByteArray()), exitCode)
+    } finally {
+        System.setErr(origErr)
+    }
+}
+
+private fun CLICompiler<*>.compile(sources: List<File>, commonSources: List<File>, vararg mainArguments: String): String = buildString {
+    val commonSourcesArgs = commonSources.map { "-Xcommon-sources=${it.absolutePath}" }
+    val multiplatformFlag = if (commonSources.isNotEmpty()) "-Xmulti-platform" else ""
+    val (output, exitCode) = executeCompiler(
+            this@compile, sources.map { it.absolutePath.toString() } + commonSources.map { it.absolutePath.toString() } +
+            commonSourcesArgs + multiplatformFlag + mainArguments
+    )
+    appendln("Exit code: $exitCode")
+    appendln("Output:")
+    appendln(output)
+}/*.trimTrailingWhitespacesAndAddNewlineAtEOF().trimEnd('\r', '\n')*/
+
+
+fun compileNative(nativeSources: List<File>, commonSources: List<File>, output: File) =
+        K2Native().compile(nativeSources, commonSources, "-p", "library", "-o", output.absolutePath.toString())
